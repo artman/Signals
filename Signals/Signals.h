e87e66616709f2c1d315b86afdc3e615c54c8be8
Tuomas Artman <tuomas@artman.fi>
2014-08-16 12:06:27 -0700

    Initial Commit

---
+++ Signals/Signals.h
@@ -0,0 +1,19 @@
+//
+//  Signals.h
+//  Signals
+//
+//  Created by Tuomas Artman on 16.8.2014.
+//  Copyright (c) 2014 Tuomas Artman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Signals.
+FOUNDATION_EXPORT double SignalsVersionNumber;
+
+//! Project version string for Signals.
+FOUNDATION_EXPORT const unsigned char SignalsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Signals/PublicHeader.h>
+
+

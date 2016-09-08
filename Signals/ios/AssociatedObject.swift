0ddf9c8e5042857b022a212eb2b4468507dd819a
Tuomas Artman <tuomas@uber.com>
2016-09-07 23:08:14 -0700

    Updated to Xcode 8 and Swift 3
    - Revamps API

--- Signals/ios/AssociatedObject.swift
+++ Signals/ios/AssociatedObject.swift
@@ -8,48 +8,22 @@
 
 import ObjectiveC
 
-#if swift(>=3.0)
-    func setAssociatedObject<T>(_ object: AnyObject, value: T, associativeKey: UnsafePointer<Void>, policy: objc_AssociationPolicy) {
-        if let valueAsAnyObject = value as? AnyObject {
-            objc_setAssociatedObject(object, associativeKey, valueAsAnyObject, policy)
-        }
-    }
+func setAssociatedObject<T>(_ object: AnyObject, value: T, associativeKey: UnsafeRawPointer, policy: objc_AssociationPolicy) {
+    objc_setAssociatedObject(object, associativeKey, value, policy)
+}
 
-    func getAssociatedObject<T>(_ object: AnyObject, associativeKey: UnsafePointer<Void>) -> T? {
-        if let valueAsType = objc_getAssociatedObject(object, associativeKey) as? T {
-            return valueAsType
-        } else {
-            return nil
-        }
+func getAssociatedObject<T>(_ object: AnyObject, associativeKey: UnsafeRawPointer) -> T? {
+    if let valueAsType = objc_getAssociatedObject(object, associativeKey) as? T {
+        return valueAsType
+    } else {
+        return nil
     }
+}
 
-    func getOrCreateAssociatedObject<T>(_ object: AnyObject, associativeKey: UnsafePointer<Void>, defaultValue:T, policy: objc_AssociationPolicy) -> T {
-        if let valueAsType: T = getAssociatedObject(object, associativeKey: associativeKey) {
-            return valueAsType
-        }
-        setAssociatedObject(object, value: defaultValue, associativeKey: associativeKey, policy: policy)
-        return defaultValue;
+func getOrCreateAssociatedObject<T>(_ object: AnyObject, associativeKey: UnsafeRawPointer, defaultValue:T, policy: objc_AssociationPolicy) -> T {
+    if let valueAsType: T = getAssociatedObject(object, associativeKey: associativeKey) {
+        return valueAsType
     }
-#else
-    func setAssociatedObject<T>(object: AnyObject, value: T, associativeKey: UnsafePointer<Void>, policy: objc_AssociationPolicy) {
-        if let valueAsAnyObject = value as? AnyObject {
-            objc_setAssociatedObject(object, associativeKey, valueAsAnyObject, policy)
-        }
-    }
-    
-    func getAssociatedObject<T>(object: AnyObject, associativeKey: UnsafePointer<Void>) -> T? {
-        if let valueAsType = objc_getAssociatedObject(object, associativeKey) as? T {
-            return valueAsType
-        } else {
-            return nil
-        }
-    }
-    
-    func getOrCreateAssociatedObject<T>(object: AnyObject, associativeKey: UnsafePointer<Void>, defaultValue:T, policy: objc_AssociationPolicy) -> T {
-        if let valueAsType: T = getAssociatedObject(object, associativeKey: associativeKey) {
-            return valueAsType
-        }
-        setAssociatedObject(object, value: defaultValue, associativeKey: associativeKey, policy: policy)
-        return defaultValue;
-    }
-#endif
+    setAssociatedObject(object, value: defaultValue, associativeKey: associativeKey, policy: policy)
+    return defaultValue;
+}
--- SignalsTests/TestListener.swift
+++ SignalsTests/TestListener.swift
@@ -1,5 +1,5 @@
 //
-//  SignalListener.swift
+//  Observer.swift
 //  Signals
 //
 //  Created by Tuomas Artman on 16.8.2014.
@@ -13,32 +13,32 @@ class TestListener {
     var dispatchCount: Int = 0;
     var lastArgument: Int = 0;
     
-    func listen(to emitter: SignalEmitter) {
-        emitter.onInt.listen(on: self, callback: {
+    func subscribe(to emitter: SignalEmitter) {
+        emitter.onInt.subscribe(on: self, callback: {
             [unowned self] (argument) in
             self.dispatchCount += 1
             self.lastArgument = argument;
         })
     }
     
-    func listenOnce(to emitter: SignalEmitter) {
-        emitter.onInt.listenOnce(on: self, callback: {
+    func subscribeOnce(to emitter: SignalEmitter) {
+        emitter.onInt.subscribeOnce(on: self, callback: {
             [unowned self] (argument) in
             self.dispatchCount += 1
             self.lastArgument = argument;
         })
     }
 
-    func listenPastOnce(to emitter: SignalEmitter) {
-        emitter.onInt.listenPastOnce(on: self, callback: {
+    func subscribePastOnce(to emitter: SignalEmitter) {
+        emitter.onInt.subscribePastOnce(on: self, callback: {
             [unowned self] (argument) in
             self.dispatchCount += 1
             self.lastArgument = argument
         })
     }
 
-    func listenPast(to emitter: SignalEmitter) {
-        emitter.onInt.listenPast(on: self, callback: {
+    func subscribePast(to emitter: SignalEmitter) {
+        emitter.onInt.subscribePast(on: self, callback: {
             [unowned self] (argument) in
             self.dispatchCount += 1
             self.lastArgument = argument;
